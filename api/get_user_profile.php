7683c3591d37601da3d714bc5562a8ca1d92ffc5
kudasai <47227786+kudasaixc@users.noreply.github.com>
2026-06-08 11:51:38 +0200

    Fix legacy profile request handling

--- api/get_user_profile.php
+++ api/get_user_profile.php
@@ -5,5 +5,6 @@ declare(strict_types=1);
 require_once __DIR__ . '/bootstrap.php';
 require_once __DIR__ . '/laravel_proxy.php';
 
+$request = new \Illuminate\Http\Request($_GET);
 $controller = laravelMake(\App\Http\Controllers\Api\GetUserProfileController::class);
-respondFromJsonResponse($controller->handle(request()));
+respondFromJsonResponse($controller->handle($request));
