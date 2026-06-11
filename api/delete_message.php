1dedd7d6ab72c8b63b1af6a0465c69587b955eb8
kudasaixc <radiopro787@gmail.com>
2026-06-11 08:19:12 +0000

    feat: route delete message through Laravel bridge

--- api/delete_message.php
+++ api/delete_message.php
@@ -1,8 +1,9 @@
 <?php
 require_once __DIR__ . '/bootstrap.php';
+require_once __DIR__ . '/laravel_proxy.php';
 
 $userId = requireAuthUserId();
 $data = json_decode(file_get_contents('php://input') ?: '', true);
 
-$controller = apiKernel()->messageController();
-respondFromController($controller->delete($userId, is_array($data) ? $data : []));
+$controller = laravelMake(\App\Http\Controllers\MessageController::class);
+respondFromJsonResponse($controller->delete($userId, is_array($data) ? $data : []));
